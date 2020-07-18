b6627fc6feda3779ae4506375b6c0dff50fcf1f5
MagusConjurer <cameronisadavis@gmail.com>
2020-07-18 13:12:27 -0600

    Add result model

---
+++ models/result.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const resultSchema = new Schema({
+  title: { type: String, required: true },
+  image: { type: String }
+});
+
+const Result = mongoose.model("Result", resultSchema);
+
+module.exports = Result;
