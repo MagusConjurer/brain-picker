79d315cc3268ae4239d8f1dbc91d16c84b8f12b3
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-07-23 20:25:28 -0700

    making files and folders for testing (Miguel)

---
+++ client/src/comp/quizCentralNav/qcNav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const heading = 
+  <div>
+    <h1>Quiz Central</h1>
+    <br /><br />
+    <h3>Quizzes</h3>
+  </div>;
+
+function QuizNav() {
+  return (
+   
+    heading
+
+  );
+}
+
+export default QuizNav;
