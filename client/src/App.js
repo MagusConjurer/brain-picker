199472014d7cd6e282bb8ec8c844c2f039a69eff
Miguel Celis <60907205+mcelis025@users.noreply.github.com>
2020-07-25 11:41:56 -0700

    created components for quiz central

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import Footer from './comp/quizCentralFooter/Footer';
+import QcNav from './comp/quizCentralNav/QcNav';
+import QuizCards from './comp/quizCentralQuizCards/QuizCards'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <QcNav />
+      <QuizCards />
+      <Footer />
     </div>
   );
 }
