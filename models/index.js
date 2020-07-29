99c2e398462b58fec3eb2a3063769869fb436df0
MagusConjurer <cameronisadavis@gmail.com>
2020-07-28 19:23:43 -0600

    Add index.js to models for exporting of all at once

---
+++ models/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  User: require("./user.js"),
+  Quiz: require("./quiz.js"),
+  Question: require("./question.js"),
+  Answer: require("./answer.js"),
+  Result: require("./result.js")
+};
