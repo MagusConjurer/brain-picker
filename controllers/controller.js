922a91b2a280eb3236ff84649ffea25bef822b72
MagusConjurer <cameronisadavis@gmail.com>
2020-07-28 19:35:15 -0600

    Change controller to better fit the MERN setup

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,19 +1,17 @@
-var express = require("express");
-var router = express.Router();
 var db = require("../models");
 
-// Default route
-router.get("/", function(req, res) {
-  // Load main page
-
-})
-
-router.get("/quiz/:id", function(req, res) {
-  // Return data for given quiz
-
-})
-
-router.get("/result/:id", function(req, res) {
-  // Return data for given result
-  
-})
+module.exports = {
+  findAll: function(req, res) {
+    db.Quiz
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbQuizzes => res.json(dbQuizzes))
+      .catch(err => res.status(422).json(err));
+  },
+  findOne: function(req, res) {
+    db.Quiz
+      .findByID(req.query)
+      .then(dbQuiz => res.json(dbQuiz))
+      .catch(err => res.status(422).json(err));
+  }
+}
