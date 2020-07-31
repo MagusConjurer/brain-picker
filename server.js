b1ceb4be0f3d187956c8035f1fd3c22094fc2f4f
Christina Lupanow <celupanow@gmail.com>
2020-07-30 19:46:40 -0600

    updating login system

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-
+const path = require("path");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Define middleware here
 
