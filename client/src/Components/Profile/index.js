dcfdaa5e82d7b4f0a91b320f0e4dde375bbff3ee
Christina Lupanow <celupanow@gmail.com>
2020-07-30 22:10:07 -0600

    fixing login

--- client/src/Components/Profile/index.js
+++ client/src/Components/Profile/index.js
@@ -15,7 +15,7 @@ class Profile extends Component {
                 <div className="row">
                     <div className="col s12 center-align">
                         <h4>
-                            <b>Hey there,</b> {user.name.split(" ")[0]}
+                            <b>Hey there,</b> {user.username}
                             <p className="flow-text grey-text text-darken-1">
                                 You are logged into a full-stack{" "}
                                 <span style={{ fontFamily: "monospace" }}>MERN</span> app 👏
