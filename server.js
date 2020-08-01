041162e7cec0c9260bef575902097201691e768d
celupanow <celupanow@gmail.com>
2020-08-01 11:27:51 -0600

    fixing login system

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const users = require("./routes/api/users");
-const routes = require("./routes");
+// const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -25,11 +25,11 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+// app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
-  process.env.DB_URI || "mongodb://localhost/brainpicker", 
+  process.env.DB_URI || "mongodb://127.0.0.1:27017/brainpicker", 
   { useNewUrlParser: true,
     useUnifiedTopology: true });
 
