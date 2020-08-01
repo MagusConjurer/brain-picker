02c4388b209bb3fdf73e4b1477aa4a8db77b0610
MagusConjurer <cameronisadavis@gmail.com>
2020-08-01 13:59:16 -0600

    Add route directly to production if

--- server.js
+++ server.js
@@ -23,6 +23,10 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  // Define any API routes before this runs
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 }
 
 // Add routes, both API and view
