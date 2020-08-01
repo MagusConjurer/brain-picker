015e056651ac02dfddb1f156856e1cb3bc6ec65a
MagusConjurer <cameronisadavis@gmail.com>
2020-08-01 14:12:32 -0600

    Change server.js order

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the built React page
-router.use("*", function(req, res) {
+router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// Add routes, both API and view
+app.use(routes);
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -28,9 +32,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 }
-
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 mongoose.connect(
