ee6c7b8e646f84375ae8d344889428aad1052934
admininstrator <ploy3_98@yahoo.com>
2020-08-04 17:56:25 -0500

    deploy

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
-  app.use(express.static("build"));
+  app.use(express.static("client/build"));
 
   // Express will serve up the front-end index.html file if it doesn't recognize the route
   app.get('*', function (req, res) {
