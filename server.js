4cffc3726f6b2f4246a0f1ee3122e2b51780c634
admininstrator <ploy3_98@yahoo.com>
2020-08-04 15:28:51 -0500

    Deploy

--- server.js
+++ server.js
@@ -10,23 +10,32 @@ const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 
-//bodyparser middleware
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-app.use(bodyParser.json());
+// //bodyparser middleware
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: false
+//   })
+// );
+// app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// }
 // Add routes, both API and view
 // app.use(routes);
 
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets
+  app.use(express.static("build"));
+
+  // Express will serve up the front-end index.html file if it doesn't recognize the route
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("build", "index.html"))
+  );
+}
 // Connect to the Mongo DB
 mongoose.connect(
   process.env.DB_URI || "mongodb://localhost/brainpicker", 
