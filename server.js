dc873960832ebe910a1f793f67cafeb19d95ec0f
admininstrator <ploy3_98@yahoo.com>
2020-08-04 17:53:07 -0500

    deploy

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === "production") {
 
   // Express will serve up the front-end index.html file if it doesn't recognize the route
   app.get('*', function (req, res) {
-    const index = path.join(__dirname, 'build', 'index.html');
+    const index = path.join(__dirname, 'client/build', 'index.html');
     res.sendFile(index);
   });
 }
