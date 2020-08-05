f2542284070366302aa942a2aa36ef99ac2f1ae8
MagusConjurer <cameronisadavis@gmail.com>
2020-08-04 19:54:54 -0600

    Re add proxy

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 // Define middleware here
 
