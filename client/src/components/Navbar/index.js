242c38e536c7a262731538b422495f29521226f3
lrdkelvin <brandonbringhurst@gmail.com>
2020-08-06 10:44:31 -0600

    fixing conflicts

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./style.css";
 
 class Navbar extends Component {
-<<<<<<< HEAD
   render() {
     return (
       <nav className="nav-wrapper">
@@ -30,22 +29,6 @@ class Navbar extends Component {
       </nav>
     );
   }
-=======
-    render() {
-        return(
-          <nav className='nav-wrapper'>
-          <div>
-            <a href='/' className='brand-logo'>Pick Their Brain</a>
-            <ul id='nav-mobile' className='right'>
-              <li><a href='/QuizCentral'>Quiz Central</a></li>
-              <li><a href='/Profile'>Profile</a></li>
-              <li><a href='/login' className='waves-effect waves-light btn'>Login</a></li>
-            </ul>
-          </div>
-        </nav>
-        )
-    }
->>>>>>> 3b48bfde64565c669c58a41294de64badabd57fb
 }
 
 export default Navbar;
--- client/src/pages/QuizCentral.js
+++ client/src/pages/QuizCentral.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD:client/src/pages/Quiz.js
 import QuizCentralHeader from '../components/quizCentralHeader';
 import QuizCards from '../components/quizCards';
-=======
-import QuizCentralHeader from '../components/QuizCentralHeader';
-import QuizCards from '../components/QuizCards';
->>>>>>> 3b48bfde64565c669c58a41294de64badabd57fb:client/src/pages/QuizCentral.js
 import Share from '../components/Share';
 import Footer from '../components/Footer';
 import QuizSample from '../QuizSamples.json';
