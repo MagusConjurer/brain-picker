e799372372076c31da4c89bfd58e09788b0c69dc
MagusConjurer <cameronisadavis@gmail.com>
2020-08-06 21:00:53 -0600

    Add props for result from the sample quiz json

--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -8,15 +8,39 @@ import QuizForm from '../components/QuizForm';
 class QuizTest extends Component {
 
     state = {
-        QuizSample
+        QuizSample,
+        submitted: false,
+        result: {},
+        labels: []
     };
+
+    getResult(resultIndex) {
+      this.setState({
+        result: QuizSample.results[resultIndex]
+      });
+    };
+
+    setLabels() {
+      quizLabels = [];
+      QuizSample.results.forEach(result => quizLabels.push(result.title));
+      this.setState({
+        labels: quizLabels
+      });
+    };
+
+    componentDidMount() {
+      this.setLabels();
+    }
 
     render() {
         return(
 
             <div>
                 <QuizPageHeader />
-                <QuizForm />
+                {submitted 
+                  ? <QuizForm />
+                  : <Result quiz={QuizSample.quizName} result={this.state.result} labels={this.state.labels} />
+                }
                 <Share className="mt-5" />
                 <Footer />
             </div>
